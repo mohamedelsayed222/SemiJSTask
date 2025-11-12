3d2b2f3efb656c8d9bc3e3ea676c933f4b50c1bb
Mohamed Elsayed <ms6701937@gmail.com>
2025-11-12 21:00:33 +0200

    Finish the task

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 import Bicycle from "./Bicycle.js";
-
+import Vehicle from "./Vehicle.js";
 let b1=new Bicycle();
 console.log(b1);
 let b2=new Bicycle("Red",2,"Quk Quk")
 console.log(b2.honkHorn());
 console.log(b2);
+let v=new Vehicle()
+console.log(v);
+
 
